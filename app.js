008dc318796a8df764081922c7dae52fe0c147df
TheAmazingBeat <38993027+TheAmazingBeat@users.noreply.github.com>
2021-01-06 21:57:21 -0600

    Update nodemon, Setup routes

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/favicon.ico', function(req, res){
+   res.sendFile(__dirname + '/favicon.ico');
+});
+
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/hw-prior.html', function(req, res){
+   res.sendFile(__dirname + '/public/src/hw-prior.html');
+});
+
+app.get('/calculator.html', function(req, res){
+   res.sendFile(__dirname + '/public/src/calculator.html');
 });
 
 app.listen(process.env.PORT || 5000, function(){
--- public/js/hw-prior.js
+++ public/js/hw-prior.js
@@ -1,33 +1,33 @@
+// jshint esversion:6
+// jshint -W030 
+
 var hwList = [],
-   hwValues = [], 
-   majorHW = [], 
+   hwValues = [],
+   majorHW = [],
    minorHW = [],
    sortedHW = [];
 var hwCounter = 0;
 
 // First three homework items
-for(let i = 0; i < 3; i++){
+for (let i = 0; i < 3; i++) {
    addHW();
 }
 
 // Today's Date
-function getDateToday(){
+function getDateToday() {
    var date = new Date();
-   var year = date.getFullYear().toString();
-   var month = (date.getMonth()+1).toString();
-   var day = date.getDate().toString();
-   var today = year + '-' + month + '-' + day;
+   var today = date.toISOString().substr(0, 10);
 
    return today;
 }
 
 // Creating Inputs for Homework Name, Due Date, and Homework Type
-function createCheckBox(){
+function createCheckBox() {
    // Creates -> <input class="hw-select mx-2" type="checkbox">
-   
+
    var checkboxCell = document.createElement('th');
-   $(checkboxCell).attr('scope', 'row')
-   $(checkboxCell).attr('class', 'hw-select-cell')
+   $(checkboxCell).attr('scope', 'row');
+   $(checkboxCell).attr('class', 'hw-select-cell');
    var checkbox = document.createElement('input');
    $(checkbox).attr('class', 'hw-select hvr-grow');
    $(checkbox).attr('type', 'checkbox');
@@ -35,10 +35,10 @@ function createCheckBox(){
    return checkboxCell;
 }
 
-function createNameInput(){
+function createNameInput() {
    // Creates -> <input class="hw-name" type="text" placeholder="Name">
    var nameCell = document.createElement('td');
-   $(nameCell).attr('class', 'hw-name-cell')
+   $(nameCell).attr('class', 'hw-name-cell');
    var nameInput = document.createElement('input');
    $(nameInput).attr('class', 'hw-name hvr-grow');
    $(nameInput).attr('type', 'text');
@@ -47,10 +47,10 @@ function createNameInput(){
    return nameCell;
 }
 
-function createDateInput(){
+function createDateInput() {
    // Creates -> <input class="hw-date" type="date" name="duedate">
    var dateInputCell = document.createElement('td');
-   $(dateInputCell).attr('class', 'hw-date-cell')
+   $(dateInputCell).attr('class', 'hw-date-cell');
    var dateInput = document.createElement('input');
    $(dateInput).attr('class', 'hw-date hvr-grow');
    $(dateInput).attr('type', 'date');
@@ -63,10 +63,10 @@ function createDateInput(){
    return dateInputCell;
 }
 
-function createTypeInput(){
+function createTypeInput() {
    // Creates -> <select name="type" id="" class="hw-type"><option value="Minor">Minor</option><option value="Major">Major</option></select>
    var typeCell = document.createElement('td');
-   $(typeCell).attr('class', 'hw-type-cell')
+   $(typeCell).attr('class', 'hw-type-cell');
    var typeInput = document.createElement('select');
    $(typeInput).attr('class', 'hw-type hvr-grow');
    $(typeInput).attr('name', 'type');
@@ -79,27 +79,27 @@ function createTypeInput(){
    $(majorOption).val('Major');
    $(majorOption).html('Major');
    $(typeInput).append(minorOption, majorOption);
-  $(typeCell).append(typeInput);
+   $(typeCell).append(typeInput);
    return typeCell;
 }
 
 
 // Getting the value of each input
-function getNameInput(index){
+function getNameInput(index) {
    return $(hwList[index]).find('.hw-name').val();
 }
 
-function getDateInput(index){
-   return  $(hwList[index]).find('.hw-date').val();
+function getDateInput(index) {
+   return $(hwList[index]).find('.hw-date').val();
 }
 
-function getTypeInput(index){
-   return  $(hwList[index]).find('.hw-type').val();
+function getTypeInput(index) {
+   return $(hwList[index]).find('.hw-type').val();
 }
 
 
 // Add a homework item in the list
-function addHW(){
+function addHW() {
    // Creates -> <li class="homework-item my-2"></li>
    var hwItem = document.createElement('tr');
    $(hwItem).attr('class', 'homework-item animate__animated animate__fadeInDown');
@@ -107,25 +107,24 @@ function addHW(){
 
    hwList.unshift(hwItem);
    hwCounter++;
-   $('table.table > tbody').prepend(hwItem);
+   $('#homework-list > tbody').prepend(hwItem);
 }
 
 // Removes the selected homework item(s) in the list
-function removeHW(){
-   if(hwCounter > 3){
-      for(let i = 0; i < hwCounter; i++){
-         if($(hwList[i]).find('.hw-select-cell').find('.hw-select').is(':checked')){
+function removeHW() {
+   if (hwCounter > 3) {
+      for (let i = 0; i < hwCounter; i++) {
+         if ($(hwList[i]).find('.hw-select-cell').find('.hw-select').is(':checked')) {
             $(hwList[i]).attr('class', 'homework-item my-2 animate__animated animate__fadeOutUp');
             $(hwList[i]).remove();
             hwList.pop();
             hwCounter--;
          }
       }
-   }
-   else{
+   } else {
       $('.alert').toggle();
    }
-   
+
    // Gets the last prepended homework item
    /*if(hwCounter > 3){
       $('ul.homework-list li:last-child').remove();
@@ -138,27 +137,28 @@ function removeHW(){
 }
 
 
-function getAllInput(){
-   for(let i = 0; i < hwCounter; i++){
-      hwValues[i] = new Object();
-      hwValues[i].name = getNameInput(i);
-      hwValues[i].date = getDateInput(i);
-      hwValues[i].type = getTypeInput(i);
-
-      hwList[i].name = getNameInput(i);
-      hwList[i].date = getDateInput(i);
-      hwList[i].type = getTypeInput(i);
+function getAllInput() {
+   for (let i = 0; i < hwCounter; i++) {
+      hwValues[i] = {
+         name: getNameInput(i),
+         date: getDateInput(i),
+         type: getTypeInput(i)
+      };
+      // hwValues[i].name = getNameInput(i);
+      // hwValues[i].date = getDateInput(i);
+      // hwValues[i].type = getTypeInput(i);
    }
 }
 
-function sortByDate(array){
-   for(let i = 0; i < array.length; i++){
+function sortByDate(array) {
+   // Selection Sort
+   for (let i = 0; i < array.length; i++) {
       var min = i;
-      for(let j = i+1; j < array.length; j++){
-         if(new Date(array[j].date) < new Date(array[i].date))
-         min = j;
+      for (let j = i + 1; j < array.length; j++) {
+         if (new Date(array[j].date) < new Date(array[i].date))
+            min = j;
       }
-      if(min != i){
+      if (min != i) {
          var temp = array[min];
          array[min] = array[i];
          array[i] = temp;
@@ -166,23 +166,22 @@ function sortByDate(array){
    }
 }
 
-function sortHW(){
+function sortHW() {
    //Counts how many MAJOR and MINOR assignments
-   for(var i = 0; i < hwCounter; i++){
-      if(hwValues[i].type == 'Major'){
+   for (var i = 0; i < hwCounter; i++) {
+      if (hwValues[i].type == 'Major') {
          majorHW.push(hwValues[i]);
-      } 
-      else{
+      } else {
          minorHW.push(hwValues[i]);
       }
    }
    // Sorts MAJOR AND MINOR assignmentsby due date
-   if(minorHW.length > 0){
+   if (minorHW.length > 0) {
       sortByDate(minorHW);
       console.log('Minor Homeworks :: ');
       console.log(minorHW);
    }
-   if(majorHW.length > 0){
+   if (majorHW.length > 0) {
       sortByDate(majorHW);
       console.log('Major Homeworks :: ');
       console.log(majorHW);
@@ -191,34 +190,70 @@ function sortHW(){
    // New array of Major Homeworks + Minor Homeworks
    sortedHW = majorHW.concat(minorHW);
 
-   for(let i = 0; i < hwCounter; i++){
+   for (let i = 0; i < hwCounter; i++) {
       var firstMajor;
-      if(sortedHW[i].type == 'Major')
+      if (sortedHW[i].type == 'Major')
          firstMajor = sortedHW[i];
-      if(sortedHW[i].type == 'Minor'){
+      if (sortedHW[i].type == 'Minor' && firstMajor != undefined) {
          /*
           * If there are more minor than major and
           * the major is due in 4 days then the minor comes first
           * 4 days = 345600000 milliseconds
           */
-         if((minorHW.length > majorHW.length) && ((new Date(firstMajor.date) - new Date(sortedHW[i].date)) >= 345600000)){
-           var temp = sortedHW[i];
-           sortedHW[i] = firstMajor;
-           sortedHW[sortedHW.indexOf(firstMajor)] = temp;
+         if ((minorHW.length > majorHW.length) && ((new Date(firstMajor.date) - new Date(sortedHW[i].date)) >= 345600000)) {
+            var temp = sortedHW[i];
+            sortedHW[i] = firstMajor;
+            sortedHW[sortedHW.indexOf(firstMajor)] = temp;
          }
       }
    }
-   
-}
-
-function showPrioritized(){
 
 }
 
-function prioritize(){
+function showPrioritized() {
+
+   hideList();
+   // prevents the table from having old list
+   $('#sorted-list > tbody').empty();
+
+   // shows the #sortedDiv when button is clicked
+   $('#sortedDiv:hidden').toggle('slow');
+
+   for (let i = 0; i < sortedHW.length; i++) {
+      var $row = $('<tr>'),
+         $numCell = $('<td>'),
+         $nameCell = $('<td>'),
+         $dateCell = $('<td>'),
+         $typeCell = $('<td>');
+
+      $($numCell).html(i + 1);
+      $($nameCell).html(sortedHW[i].name);
+      $($dateCell).html(sortedHW[i].date);
+      $($typeCell).html(sortedHW[i].type);
+
+      $($row).append($numCell, $nameCell, $dateCell, $typeCell);
+
+      $('#sorted-list > tbody').append($row);
+   }
+}
+
+// hides where the user edits homework list
+function hideList(){
+   $('#userDiv:visible').toggle('slow');
+}
+
+function edit(){
+   $('#sortedDiv:visible').toggle('slow');
+   $('#userDiv:hidden').toggle('slow');
+}
+
+function prioritize() {
+   // resets arrays
    hwValues = [],
-   majorHW = [],
-   minorHW = [];
-   getAllInput();
-   sortHW();
+      majorHW = [],
+      minorHW = [];
+
+   getAllInput(),
+   sortHW(),
+   showPrioritized();
 }
