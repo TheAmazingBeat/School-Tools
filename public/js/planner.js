be216a823a3c447840ce637f7556aa6f092a529d
TheAmazingBeat <38993027+TheAmazingBeat@users.noreply.github.com>
2021-05-23 21:30:34 -0500

    Render days in calendar

--- public/js/planner.js
+++ public/js/planner.js
@@ -1,52 +1,109 @@
 //jshint esversion:6
 
-let d = new Date();
+const months = ['January', 'February', 'March', 'April', 'May', 'June',
+   'July', 'August', 'September', 'October', 'November', 'December'
+];
 
-$(document).ready(function(){
-   $('#monthYear').text(getCurrentMonth() + ' ' + d.getFullYear());
+let d = new Date();
+let currMonth = d.getMonth();
+let currYear = d.getFullYear();
+let monthNum = d.getMonth();
+let yearNum = d.getFullYear();
+let $calendar = $('#monthYear');
+
+$(document).ready(function () {
+   $calendar.text(getRealMonth() + ' ' + yearNum);
+   loadCalendarDays();
 });
 
-function getCurrentMonth() {
+function getRealMonth() {
+   return getTheMonth(d.getMonth());
+}
+
+function getTheMonth(num) {
    let month;
 
-   switch(d.getMonth()){
-      case 0:
-         month = 'January';
-         break;
-      case 1:
-         month = 'February';
-         break;
-      case 2:
-         month = 'March';
-         break;
-      case 3:
-         month = 'April';
-         break;
-      case 4:
-         month = 'May';
-         break;
-      case 5:
-         month = 'June';
-         break;
-      case 6:
-         month = 'July';
-         break;
-      case 7:
-         month = 'August';
-         break;
-      case 8:
-         month = 'September';
-         break;
-      case 9:
-         month = 'October';
-         break;
-      case 10:
-         month = 'November';
-         break;
-      case 11:
-         month = 'December';
-         break;
+   if (num < 0)
+      num = 11;
+   else if (num > 11)
+      num = 0;
+
+   month = months[num];
+   return month;
+}
+
+function previous() {
+   //Indicates year before
+   if (getTheMonth(monthNum - 1) == months[11])
+      yearNum--;
+
+
+   if ((monthNum - 1) < 0) {
+      monthNum = 11;
+      $calendar.text(getTheMonth(monthNum) + ' ' + yearNum);
+   } else {
+      $calendar.text(getTheMonth(monthNum - 1) + ' ' + yearNum);
+      monthNum--;
    }
 
-   return month;
+   loadCalendarDays();
+}
+
+function next() {
+   //Indicates new year
+   if (getTheMonth(monthNum + 1) == months[0])
+      yearNum++;
+
+
+   if ((monthNum + 1) > 11) {
+      monthNum = 0;
+      $calendar.text(getTheMonth(monthNum) + ' ' + yearNum);
+   } else {
+      $calendar.text(getTheMonth(monthNum + 1) + ' ' + yearNum);
+      monthNum++;
+   }
+
+   loadCalendarDays();
+
+}
+
+//Returns the first weekday of the month.
+function numOfDays(month, year) {
+   let d = new Date(year, month + 1, 0);
+   return d.getDate();
+}
+
+function loadCalendarDays() {
+   $('#calendarDays').html('');
+
+   let tmpDate = new Date(currYear, currMonth, 0);
+   let num = numOfDays(currMonth, currYear);
+   //Gets the first day of the month
+   let dayofweek = tmpDate.getDay();
+
+   // create day prefixes before first day of the month
+   for (let i = 0; i <= dayofweek; i++) {
+      let d = document.createElement("div");
+      $(d).attr('class', 'day blank');
+      $('#calendarDays').append(d);
+   }
+
+   // creates rest of the days in the month
+   for (let i = 0; i < num; i++) {
+      let dayNum = i + 1;
+      let d = document.createElement("div");
+      $(d).attr('id', 'calendarDay_' + i)
+      $(d).attr('class', 'day');
+
+      // text box inside div
+      let n = document.createElement('div');
+      $(n).attr('class', 'day-num');
+      $(n).text(dayNum);
+      $(d).append(n);
+      $('#calendarDays').append(d);
+   }
+
+   var clear = document.createElement("div");
+   clear.className = "clear";
+   document.getElementById("calendarDays").appendChild(clear);
 }
