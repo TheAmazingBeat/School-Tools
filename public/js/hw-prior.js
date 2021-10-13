6bd3b5be7b02a2f4b1ff9fe2a237efc11d0dba5a
Isaac Baldivas <38993027+TheAmazingBeat@users.noreply.github.com>
2021-10-13 14:00:02 +0000

    Display Stored Homeworks from  hw-prior.js to modal in planner.html

--- public/js/hw-prior.js
+++ public/js/hw-prior.js
@@ -1,139 +1,153 @@
 let hwList = [],
-  hwValues = [],
-  majorHW = [],
-  minorHW = [],
-  sortedHW = [];
+	hwValues = [],
+	majorHW = [],
+	minorHW = [],
+	sortedHW = [];
 let hwCounter = 0;
 
 // First three homework items
 for (let i = 0; i < 3; i++) {
-  addHW();
+	addHW();
 }
 
 // Today's Date
 function getDateToday() {
-  let date = new Date();
-  let today = date.toISOString().substr(0, 10);
+	let date = new Date();
+	let today = date.toISOString().substr(0, 10);
 
-  return today;
+	return today;
 }
 
 // Creating Inputs for Homework Name, Due Date, and Homework Type
 function createCheckBox() {
-  /// Creates -> <input class="hw-select mx-2" type="checkbox">
+	/// Creates -> <input class="hw-select mx-2" type="checkbox">
 
-  let checkboxCell = document.createElement("th");
-  $(checkboxCell).attr("scope", "row");
-  $(checkboxCell).attr("class", "hw-select-cell");
-  let checkbox = document.createElement("input");
-  $(checkbox).attr("class", "hw-select hvr-grow");
-  $(checkbox).attr("type", "checkbox");
-  $(checkboxCell).append(checkbox);
-  return checkboxCell;
+	let checkboxCell = document.createElement("th");
+	$(checkboxCell).attr("scope", "row");
+	$(checkboxCell).attr("class", "hw-select-cell");
+	let checkbox = document.createElement("input");
+	$(checkbox).attr("class", "hw-select hvr-grow");
+	$(checkbox).attr("type", "checkbox");
+	$(checkboxCell).append(checkbox);
+	return checkboxCell;
 }
 
 function createNameInput() {
-  /// Creates -> <input class="hw-name" type="text" placeholder="Name">
-  let nameCell = document.createElement("td");
-  $(nameCell).attr("class", "hw-name-cell");
-  let nameInput = document.createElement("input");
-  $(nameInput).attr("class", "hw-name hvr-grow");
-  $(nameInput).attr("type", "text");
-  $(nameInput).attr("placeholder", "Homework Item");
-  $(nameCell).append(nameInput);
-  return nameCell;
+	/// Creates -> <input class="hw-name" type="text" placeholder="Name">
+	let nameCell = document.createElement("td");
+	$(nameCell).attr("class", "hw-name-cell");
+	let nameInput = document.createElement("input");
+	$(nameInput).attr("class", "hw-name hvr-grow");
+	$(nameInput).attr("type", "text");
+	$(nameInput).attr("placeholder", "Homework Item");
+	$(nameCell).append(nameInput);
+	return nameCell;
 }
 
 function createDateInput() {
-  /// Creates -> <input class="hw-date" type="date" name="duedate">
-  let dateInputCell = document.createElement("td");
-  $(dateInputCell).attr("class", "hw-date-cell");
-  let dateInput = document.createElement("input");
-  $(dateInput).attr("class", "hw-date hvr-grow");
-  $(dateInput).attr("type", "date");
-  $(dateInput).attr("name", "duedate");
+	/// Creates -> <input class="hw-date" type="date" name="duedate">
+	let dateInputCell = document.createElement("td");
+	$(dateInputCell).attr("class", "hw-date-cell");
+	let dateInput = document.createElement("input");
+	$(dateInput).attr("class", "hw-date hvr-grow");
+	$(dateInput).attr("type", "date");
+	$(dateInput).attr("name", "duedate");
 
-  // Sets the initial value to today's date
-  $(dateInput).val(getDateToday());
+	// Sets the initial value to today's date
+	$(dateInput).val(getDateToday());
 
-  $(dateInputCell).append(dateInput);
-  return dateInputCell;
+	$(dateInputCell).append(dateInput);
+	return dateInputCell;
 }
 
 function createTypeInput() {
-  /// Creates -> <select name="type" id="" class="hw-type"><option value="Minor">Minor</option><option value="Major">Major</option></select>
-  let typeCell = document.createElement("td");
-  $(typeCell).attr("class", "hw-type-cell");
-  let typeInput = document.createElement("select");
-  $(typeInput).attr("class", "hw-type hvr-grow");
-  $(typeInput).attr("name", "type");
-  // Minor in dropdown
-  let minorOption = document.createElement("option");
-  $(minorOption).val("Minor");
-  $(minorOption).html("Minor");
-  // Major in dropdown
-  let majorOption = document.createElement("option");
-  $(majorOption).val("Major");
-  $(majorOption).html("Major");
-  $(typeInput).append(minorOption, majorOption);
-  $(typeCell).append(typeInput);
-  return typeCell;
+	/// Creates -> <select name="type" id="" class="hw-type"><option value="Minor">Minor</option><option value="Major">Major</option></select>
+	let typeCell = document.createElement("td");
+	$(typeCell).attr("class", "hw-type-cell");
+	let typeInput = document.createElement("select");
+	$(typeInput).attr("class", "hw-type hvr-grow");
+	$(typeInput).attr("name", "type");
+	// Minor in dropdown
+	let minorOption = document.createElement("option");
+	$(minorOption).val("Minor");
+	$(minorOption).html("Minor");
+	// Major in dropdown
+	let majorOption = document.createElement("option");
+	$(majorOption).val("Major");
+	$(majorOption).html("Major");
+	$(typeInput).append(minorOption, majorOption);
+	$(typeCell).append(typeInput);
+	return typeCell;
 }
 
 // Getting the value of each input
 function getNameInput(index) {
-  return $(hwList[index]).find(".hw-name").val();
+	return $(hwList[index]).find(".hw-name").val();
 }
 
 function getDateInput(index) {
-  return $(hwList[index]).find(".hw-date").val();
+	/// Initial Format YYYY-MM-DD
+	let dateString = $(hwList[index]).find(".hw-date").val();
+
+	/// Format to MM-DD-YYYY
+	let month = dateString.substring(dateString.indexOf('-')+1, dateString.indexOf('-', dateString.indexOf('-')+1));
+	//// Removes ZERO in the first digit of month
+	if(month.substring(0,1) == '0' && month.length > 1){
+		month = month.substring(1);
+	}
+	let day = dateString.substring(dateString.indexOf('-', dateString.indexOf('-')+1)+1)
+	let year = dateString.substring(0,4)
+	
+	let formattedDate = month + '-' + day + '-' + year;
+
+	return formattedDate;
 }
 
 function getTypeInput(index) {
-  return $(hwList[index]).find(".hw-type").val();
+	return $(hwList[index]).find(".hw-type").val();
 }
 
 // Add a homework item in the list
 function addHW() {
-  /// Creates -> <li class="homework-item my-2"></li>
-  let hwItem = document.createElement("tr");
-  $(hwItem).attr(
-    "class",
-    "homework-item animate__animated animate__fadeInDown"
-  );
-  $(hwItem).append(
-    createCheckBox(),
-    createNameInput(),
-    createDateInput(),
-    createTypeInput()
-  );
+	/// Creates -> <li class="homework-item my-2"></li>
+	let hwItem = document.createElement("tr");
+	$(hwItem).attr(
+		"class",
+		"homework-item animate__animated animate__fadeInDown"
+	);
+	$(hwItem).append(
+		createCheckBox(),
+		createNameInput(),
+		createDateInput(),
+		createTypeInput()
+	);
 
-  hwList.unshift(hwItem);
-  hwCounter++;
-  $("#homework-list > tbody").prepend(hwItem);
+	hwList.unshift(hwItem);
+	hwCounter++;
+	$("#homework-list > tbody").prepend(hwItem);
 }
 
 // Removes the selected homework item(s) in the list
 function removeHW() {
-  if (hwCounter > 3) {
-    for (let i = 0; i < hwCounter; i++) {
-      if (
-        $(hwList[i]).find(".hw-select-cell").find(".hw-select").is(":checked")
-      ) {
-        $(hwList[i]).attr(
-          "class",
-          "homework-item my-2 animate__animated animate__fadeOutUp"
-        );
-        $(hwList[i]).remove();
-        hwList.pop();
-        hwCounter--;
-      }
-    }
-  } else {
-    $(".alert").toggle();
-  }
+	if (hwCounter > 3) {
+		for (let i = 0; i < hwCounter; i++) {
+			if (
+				$(hwList[i]).find(".hw-select-cell").find(".hw-select").is(":checked")
+			) {
+				$(hwList[i]).attr(
+					"class",
+					"homework-item my-2 animate__animated animate__fadeOutUp"
+				);
+				$(hwList[i]).remove();
+				hwList.pop();
+				hwCounter--;
+			}
+		}
+	} else {
+		$(".alert").toggle();
+	}
 
-  // Gets the last prepended homework item
+	// Gets the last prepended homework item
   /*if(hwCounter > 3){
       $('ul.homework-list li:last-child').remove();
       hwList.pop();
@@ -145,123 +159,123 @@ function removeHW() {
 }
 
 function getAllInput() {
-  for (let i = 0; i < hwCounter; i++) {
-    hwValues[i] = {
-      name: getNameInput(i),
-      date: getDateInput(i),
-      type: getTypeInput(i),
-    };
-  }
+	for (let i = 0; i < hwCounter; i++) {
+		hwValues[i] = {
+			name: getNameInput(i),
+			date: getDateInput(i),
+			type: getTypeInput(i),
+		};
+	}
 }
 
 // Sorts array from closest date to farthest date
 function sortByDate(array) {
-  /// Selection Sort
-  for (let i = 0; i < array.length; i++) {
-    let min = i;
-    for (let j = i + 1; j < array.length; j++) {
-      if (new Date(array[j].date) < new Date(array[i].date)) min = j;
-    }
-    if (min != i) {
-      let temp = array[min];
-      array[min] = array[i];
-      array[i] = temp;
-    }
-  }
+	/// Selection Sort
+	for (let i = 0; i < array.length; i++) {
+		let min = i;
+		for (let j = i + 1; j < array.length; j++) {
+			if (new Date(array[j].date) < new Date(array[i].date)) min = j;
+		}
+		if (min != i) {
+			let temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+		}
+	}
 }
 
 function sortHW() {
-  /// Counts how many MAJOR and MINOR assignments
-  for (let i = 0; i < hwCounter; i++) {
-    if (hwValues[i].type == "Major") {
-      majorHW.push(hwValues[i]);
-    } else {
-      minorHW.push(hwValues[i]);
-    }
-  }
+	/// Counts how many MAJOR and MINOR assignments
+	for (let i = 0; i < hwCounter; i++) {
+		if (hwValues[i].type == "Major") {
+			majorHW.push(hwValues[i]);
+		} else {
+			minorHW.push(hwValues[i]);
+		}
+	}
 
-  /// Sorts MAJOR AND MINOR assignments by due date
-  if (minorHW.length > 0) {
-    sortByDate(minorHW);
-    console.log("Minor Homeworks :: ");
-    console.log(minorHW);
-  }
-  if (majorHW.length > 0) {
-    sortByDate(majorHW);
-    console.log("Major Homeworks :: ");
-    console.log(majorHW);
-  }
+	/// Sorts MAJOR AND MINOR assignments by due date
+	if (minorHW.length > 0) {
+		sortByDate(minorHW);
+		console.log("Minor Homeworks :: ");
+		console.log(minorHW);
+	}
+	if (majorHW.length > 0) {
+		sortByDate(majorHW);
+		console.log("Major Homeworks :: ");
+		console.log(majorHW);
+	}
 
-  /// New array of Major Homeworks + Minor Homeworks
-  sortedHW = majorHW.concat(minorHW);
+	/// New array of Major Homeworks + Minor Homeworks
+	sortedHW = majorHW.concat(minorHW);
 
-  /// Prioritization between major and minor homeworks based on quantity and date
-  for (let i = 0; i < hwCounter; i++) {
-    let firstMajor;
-    if (sortedHW[i].type == "Major") firstMajor = sortedHW[i];
-    if (sortedHW[i].type == "Minor" && firstMajor != undefined) {
+	/// Prioritization between major and minor homeworks based on quantity and date
+	for (let i = 0; i < hwCounter; i++) {
+		let firstMajor;
+		if (sortedHW[i].type == "Major") firstMajor = sortedHW[i];
+		if (sortedHW[i].type == "Minor" && firstMajor != undefined) {
       /*
        * If there are more minor than major and
        * the major is due in 4 days then the minor comes first
        * 4 days = 345600000 milliseconds
        */
-      if (
-        minorHW.length > majorHW.length &&
-        new Date(firstMajor.date) - new Date(sortedHW[i].date) >= 345600000
-      ) {
-        let temp = sortedHW[i];
-        sortedHW[i] = firstMajor;
-        sortedHW[sortedHW.indexOf(firstMajor)] = temp;
-      }
-    }
-  }
+			if (
+				minorHW.length > majorHW.length &&
+				new Date(firstMajor.date) - new Date(sortedHW[i].date) >= 345600000
+			) {
+				let temp = sortedHW[i];
+				sortedHW[i] = firstMajor;
+				sortedHW[sortedHW.indexOf(firstMajor)] = temp;
+			}
+		}
+	}
 }
 
 function showPrioritized() {
-  hideList();
-  // prevents the table from having old list
-  $("#sorted-list > tbody").empty();
+	hideList();
+	// prevents the table from having old list
+	$("#sorted-list > tbody").empty();
 
-  // shows the #sortedDiv when button is clicked
-  $("#sortedDiv:hidden").toggle("slow");
+	// shows the #sortedDiv when button is clicked
+	$("#sortedDiv:hidden").toggle("slow");
 
-  for (let i = 0; i < sortedHW.length; i++) {
-    let $row = $("<tr>"),
-      $numCell = $("<td>"),
-      $nameCell = $("<td>"),
-      $dateCell = $("<td>"),
-      $typeCell = $("<td>");
+	for (let i = 0; i < sortedHW.length; i++) {
+		let $row = $("<tr>"),
+			$numCell = $("<td>"),
+			$nameCell = $("<td>"),
+			$dateCell = $("<td>"),
+			$typeCell = $("<td>");
 
-    $($numCell).html(i + 1);
-    $($nameCell).html(sortedHW[i].name);
-    $($dateCell).html(sortedHW[i].date);
-    $($typeCell).html(sortedHW[i].type);
+		$($numCell).html(i + 1);
+		$($nameCell).html(sortedHW[i].name);
+		$($dateCell).html(sortedHW[i].date);
+		$($typeCell).html(sortedHW[i].type);
 
-    $($row).append($numCell, $nameCell, $dateCell, $typeCell);
+		$($row).append($numCell, $nameCell, $dateCell, $typeCell);
 
-    $("#sorted-list > tbody").append($row);
-  }
+		$("#sorted-list > tbody").append($row);
+	}
 }
 
-function storeHW(){
-  let homeworks = JSON.stringify(sortedHW);
-  localStorage.setItem('homeworks', homeworks);
-  console.log('Stored Homeworks');
+function storeHW() {
+	let homeworks = JSON.stringify(sortedHW);
+	localStorage.setItem('homeworks', homeworks);
+	console.log('%c Successfully Stored Homeworks!', 'color:green;');
 }
 
 // hides where the user edits homework list
 function hideList() {
-  $("#userDiv:visible").toggle("slow");
+	$("#userDiv:visible").toggle("slow");
 }
 
 function edit() {
-  $("#sortedDiv:visible").toggle("slow");
-  $("#userDiv:hidden").toggle("slow");
+	$("#sortedDiv:visible").toggle("slow");
+	$("#userDiv:hidden").toggle("slow");
 }
 
 function prioritize() {
-  // resets arrays
-  (hwValues = []), (majorHW = []), (minorHW = []);
+	// resets arrays
+	(hwValues = []), (majorHW = []), (minorHW = []);
 
-  getAllInput(), sortHW(), showPrioritized(), storeHW();
+	getAllInput(), sortHW(), showPrioritized(), storeHW();
 }
--- public/js/planner.js
+++ public/js/planner.js
@@ -1,16 +1,16 @@
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
 ];
 let d = new Date();
 let monthNum = d.getMonth();
@@ -20,225 +20,243 @@ let animateDelay = 1500;
 let days;
 
 // Beginning at the current month and year
-$(document).ready(function () {
-  $calendar.text(getTheMonth(monthNum) + " " + yearNum);
-  // dateLog();
-  loadCalendarDays();
-  highlightToday();
+$(document).ready(function() {
+	$calendar.text(getTheMonth(monthNum) + " " + yearNum);
+	// dateLog();
+	loadCalendarDays();
+	highlightToday();
 });
 
 // Highlight current date
 function highlightToday() {
-  let dateString = (d.getMonth()+1) + "-" + d.getDate() + "-" + yearNum;
-  let idStem = "calendarMonthDayYear_";
-  let selector = "#" + idStem + dateString;
-  console.log("Today's Date DOM Selector: " + selector);
-  $(selector).addClass("today");
+	let dateString = (d.getMonth() + 1) + "-" + d.getDate() + "-" + yearNum;
+	let idStem = "calendarMonthDayYear_";
+	let selector = "#" + idStem + dateString;
+	console.log("Today's Date DOM Selector: " + selector);
+	$(selector).addClass("today");
 }
 
 function dateLog() {
-  console.log("%c Date:", "color: green; font-weight: bold;");
-  console.log({
-    monthNum,
-    yearNum,
-  });
+	console.log("%c Date:", "color: green; font-weight: bold;");
+	console.log({
+		monthNum,
+		yearNum,
+	});
 }
 
 // Get month from months array
 function getTheMonth(num) {
-  if (num < 0) num = 11;
-  else if (num > 11) num = 0;
+	if (num < 0) num = 11;
+	else if (num > 11) num = 0;
 
-  return months[num];
+	return months[num];
 }
 
 function previous() {
-  animateDelay = 0;
-  // Indicates year before
-  if (getTheMonth(monthNum - 1) == months[11]) yearNum--;
+	animateDelay = 0;
+	// Indicates year before
+	if (getTheMonth(monthNum - 1) == months[11]) yearNum--;
 
-  //Month before
-  if (monthNum - 1 < 0) {
-    //December <- January
-    monthNum = 11;
-    $calendar.text(getTheMonth(monthNum) + " " + yearNum);
-  } else {
-    $calendar.text(getTheMonth(monthNum - 1) + " " + yearNum);
-    monthNum--;
-  }
+	//Month before
+	if (monthNum - 1 < 0) {
+		//December <- January
+		monthNum = 11;
+		$calendar.text(getTheMonth(monthNum) + " " + yearNum);
+	} else {
+		$calendar.text(getTheMonth(monthNum - 1) + " " + yearNum);
+		monthNum--;
+	}
 
-  $calendar.attr("class", "animate__animated animate__fadeInLeft");
-  $calendar.on("animationend", function () {
-    $calendar.attr("class", "");
-  });
+	$calendar.attr("class", "animate__animated animate__fadeInLeft");
+	$calendar.on("animationend", function() {
+		$calendar.attr("class", "");
+	});
 
-  dateLog();
-  loadCalendarDays();
-  highlightToday();
+	dateLog();
+	loadCalendarDays();
+	highlightToday();
 }
 
 function next() {
-  animateDelay = 0;
-  // Indicates new year
-  if (getTheMonth(monthNum + 1) == months[0]) yearNum++;
+	animateDelay = 0;
+	// Indicates new year
+	if (getTheMonth(monthNum + 1) == months[0]) yearNum++;
 
-  // Month after
-  if (monthNum + 1 > 11) {
-    //December -> January
-    monthNum = 0;
-    $calendar.text(getTheMonth(monthNum) + " " + yearNum);
-  } else {
-    $calendar.text(getTheMonth(monthNum + 1) + " " + yearNum);
-    monthNum++;
-  }
+	// Month after
+	if (monthNum + 1 > 11) {
+		//December -> January
+		monthNum = 0;
+		$calendar.text(getTheMonth(monthNum) + " " + yearNum);
+	} else {
+		$calendar.text(getTheMonth(monthNum + 1) + " " + yearNum);
+		monthNum++;
+	}
 
-  $calendar.attr("class", "animate__animated animate__fadeInRight");
-  $calendar.on("animationend", function () {
-    $calendar.attr("class", "");
-  });
+	$calendar.attr("class", "animate__animated animate__fadeInRight");
+	$calendar.on("animationend", function() {
+		$calendar.attr("class", "");
+	});
 
-  dateLog();
-  loadCalendarDays();
-  highlightToday();
+	dateLog();
+	loadCalendarDays();
+	highlightToday();
 }
 
 // Returns the number of days in the month
 function numOfDays(month, year) {
-  let d = new Date(year, month + 1, 0);
-  return d.getDate();
+	let d = new Date(year, month + 1, 0);
+	return d.getDate();
 }
 
 function loadCalendarDays() {
-  $("#calendarDays").html("");
-  days = 0;
+	$("#calendarDays").html("");
+	days = 0;
 
-  let tmpDate = new Date(yearNum, monthNum, 1);
-  // Gets how many days in the month
-  let num = numOfDays(monthNum, yearNum);
-  // Gets the first day of the month
-  let dayOfWeek = tmpDate.getDay();
+	let tmpDate = new Date(yearNum, monthNum, 1);
+	// Gets how many days in the month
+	let num = numOfDays(monthNum, yearNum);
+	// Gets the first day of the month
+	let dayOfWeek = tmpDate.getDay();
 
-  console.log("%c loadCalendarDays():", "color:white; font-weight:bold;");
-  console.log({
-    tmpDate,
-    num,
-    dayOfWeek,
-  });
+	console.log("%c loadCalendarDays():", "color:blue; font-weight:bold;");
+	console.log({
+		tmpDate,
+		num,
+		dayOfWeek,
+	});
 
-  // create day prefixes before first day of the month
-  for (let i = dayOfWeek; i > 0; i--) {
-    createDayCells("blank-begin", i, tmpDate);
-    days++;
-  }
-  // creates rest of the days in the month
-  for (let i = 0; i < num; i++) {
-    createDayCells("real", i);
-    days++;
-  }
-  // create blank days after last day of the month
-  let daysLeft;
+	// create day prefixes before first day of the month
+	for (let i = dayOfWeek; i > 0; i--) {
+		createDayCells("blank-begin", i, tmpDate);
+		days++;
+	}
+	// creates rest of the days in the month
+	for (let i = 0; i < num; i++) {
+		createDayCells("real", i);
+		days++;
+	}
+	// create blank days after last day of the month
+	let daysLeft;
 
-  if (days > 35) daysLeft = 42 - days;
-  else daysLeft = 35 - days;
+	if (days > 35) daysLeft = 42 - days;
+	else daysLeft = 35 - days;
 
-  if (days != 35) {
-    for (let i = 0; i < daysLeft; i++) {
-      createDayCells("blank-end", i);
-    }
-  }
+	if (days != 35) {
+		for (let i = 0; i < daysLeft; i++) {
+			createDayCells("blank-end", i);
+		}
+	}
 }
 
 // Makes days in the calendar
 function createDayCells(type, index) {
-  let idStem = "calendarMonthDayYear_";
-  let id = "";
+	let idStem = "calendarMonthDayYear_";
+	let id = "";
 
-  // Base day cell
-  let d = document.createElement("div");
-  $(d).addClass("day animate__animated animate__fadeIn");
-  $(d).css("animation-delay", animateDelay.toString() + "ms");
-  $(d).attr("data-bs-toggle", "modal");
-  $(d).attr("data-bs-target", "#dayDetails");
+	// Base day cell
+	let d = document.createElement("div");
+	$(d).addClass("day animate__animated animate__fadeIn");
+	$(d).css("animation-delay", animateDelay.toString() + "ms");
+	$(d).attr("data-bs-toggle", "modal");
+	$(d).attr("data-bs-target", "#dayDetails");
 
-  // Blank cells before the current month
-  if (type == "blank-begin") {
-    let daysBefore = numOfDays(monthNum - 1, yearNum) - (index - 1);
-    id = monthNum + "-" + daysBefore + "-" + yearNum;
-    $(d).attr("id", idStem + id);
-    $(d).addClass("blank");
-    $(d).attr("data-bs-whatever", id);
+	// Blank cells before the current month
+	if (type == "blank-begin") {
+		let daysBefore = numOfDays(monthNum - 1, yearNum) - (index - 1);
+		id = monthNum + "-" + daysBefore + "-" + yearNum;
+		$(d).attr("id", idStem + id);
+		$(d).addClass("blank");
+		$(d).attr("data-bs-whatever", id);
 
-    // text inside div
-    let n = document.createElement("div");
-    $(n).attr("class", "day-num");
-    $(n).text(daysBefore);
-    $(d).append(n);
-  }
+		// text inside div
+		let n = document.createElement("div");
+		$(n).attr("class", "day-num");
+		$(n).text(daysBefore);
+		$(d).append(n);
+	}
 
-  // Blank cells after the current month
-  if (type == "blank-end") {
-    id = monthNum + 2 + "-" + (index + 1) + "-" + yearNum;
-    $(d).attr("id", idStem + id);
-    $(d).addClass("blank");
-    $(d).attr("data-bs-whatever", id);
+	// Blank cells after the current month
+	if (type == "blank-end") {
+		id = monthNum + 2 + "-" + (index + 1) + "-" + yearNum;
+		$(d).attr("id", idStem + id);
+		$(d).addClass("blank");
+		$(d).attr("data-bs-whatever", id);
 
-    // text inside div
-    let daysAfter = index + 1;
-    let n = document.createElement("div");
-    $(n).attr("class", "day-num");
-    $(n).text(daysAfter);
-    $(d).append(n);
-  }
+		// text inside div
+		let daysAfter = index + 1;
+		let n = document.createElement("div");
+		$(n).attr("class", "day-num");
+		$(n).text(daysAfter);
+		$(d).append(n);
+	}
 
-  //days in the current month
-  if (type == "real") {
-    id = monthNum + 1 + "-" + (index + 1) + "-" + yearNum;
-    $(d).attr("id", idStem + id);
-    $(d).attr("data-bs-whatever", id);
+	//days in the current month
+	if (type == "real") {
+		id = monthNum + 1 + "-" + (index + 1) + "-" + yearNum;
+		$(d).attr("id", idStem + id);
+		$(d).attr("data-bs-whatever", id);
 
-    // text inside div
-    let dayNum = index + 1;
-    let n = document.createElement("div");
-    $(n).attr("class", "day-num");
-    $(n).text(dayNum);
-    $(d).append(n);
-  }
+		// text inside div
+		let dayNum = index + 1;
+		let n = document.createElement("div");
+		$(n).attr("class", "day-num");
+		$(n).text(dayNum);
+		$(d).append(n);
+	}
 
-  $("#calendarDays").append(d);
-  animateDelay += 20;
+	$("#calendarDays").append(d);
+	animateDelay += 20;
 }
 
 // Day Details when clicked on a day
 let $modal = $("#dayDetails");
-$modal.on("show.bs.modal", function (event) {
-  /// Div that triggered the modal
-  let button = event.relatedTarget;
+$modal.on("show.bs.modal", function(event) {
+	/// Div that triggered the modal
+	let button = event.relatedTarget;
 
-  /// Extract info from data-bs-* attributes
-  let date = button.getAttribute("data-bs-whatever");
-  
-  /// Update the modal's content.
-  let $modalTitle = $(".modal-title");
+	/// Extract info from data-bs-* attributes
+	let date = button.getAttribute("data-bs-whatever");
 
-  /// Date formatting on modal header
+	/// Update the modal's content.
+	let $modalTitle = $(".modal-title");
+
+	/// Date formatting on modal header
 	let formatDate = () => {
 		let someDate = date.toString();
-		let firstNum = someDate.substring(0, someDate.indexOf('-'));
-		let secondNum = someDate.substring((date.indexOf('-')+1), someDate.indexOf('-', someDate.indexOf('-')+1)) 
-		let thirdNum = someDate.substring(someDate.indexOf('-', (someDate.indexOf('-')+1))+1);
+		let month = someDate.substring(0, someDate.indexOf('-'));
+		let day = someDate.substring((date.indexOf('-') + 1), someDate.indexOf('-', someDate.indexOf('-') + 1))
+		let year = someDate.substring(someDate.indexOf('-', (someDate.indexOf('-') + 1)) + 1);
 
-		return getTheMonth(firstNum-1)+ ' ' + secondNum + ', ' +thirdNum;
+		return getTheMonth(month - 1) + ' ' + day + ', ' + year;
 	}
-  $modalTitle.text(formatDate);
+	$modalTitle.text(formatDate);
+
+	displayStoredHW(date);
 });
 
 // Add Event
 let dayEvent = {
-  name: "",
-  date: "",
+	name: "",
+	date: "",
 };
 
-function addCalendarEvent() {}
+// Add Event Form
+function addCalendarEvent() { }
 
+
+// Stored HW
 let homeworks = JSON.parse(localStorage.getItem('homeworks'));
-console.log({homeworks});
+// Display Homeworks from Prioritizer
+function displayStoredHW(selectedDate){
+	$('#schoolWorkList').text('');
+	// $('#schoolWorkList').text(homeworks);
+	console.log('Selected Date: ' + selectedDate);
+	/// Analyze/Format objects
+	for(let i = 0; i<homeworks.length; i++){
+		let schoolWorkItem = document.createElement('li');
+		$(schoolWorkItem).addClass('school-work-item');
+		$(schoolWorkItem).text(homeworks[i].name + ' (' + homeworks[i].type + ' Grade' + ')');
+		if(homeworks[i].date == selectedDate)
+			$('#schoolWorkList').append(schoolWorkItem);
+	}
+}
