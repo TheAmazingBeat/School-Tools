63e62701829cddb1dd67838990fd7a3e40bba6b8
TheAmazingBeat <ice.baldivas@gmail.com>
2022-02-04 23:30:14 -0600

    Improve routes

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const express = require("express");
+const express = require('express');
 const fs = require('fs');
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/favicon.ico", function (req, res) {
-  res.sendFile(__dirname + "/favicon.ico");
+app.get('/favicon.ico', function (req, res) {
+	res.sendFile(__dirname + '/favicon.ico');
 });
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-app.get("/hw-prior.html", function (req, res) {
-  res.sendFile(__dirname + "/public/src/hw-prior.html");
+app.get('/prioritizer', function (req, res) {
+	res.sendFile(__dirname + '/public/src/prioritizer.html');
 });
 
-app.get("/calculator.html", function (req, res) {
-  res.sendFile(__dirname + "/public/src/calculator.html");
+app.get('/calculator', function (req, res) {
+	res.sendFile(__dirname + '/public/src/calculator.html');
 });
 
-app.get("/planner.html", function (req, res) {
-  res.sendFile(__dirname + "/public/src/planner.html");
+app.get('/planner', function (req, res) {
+	res.sendFile(__dirname + '/public/src/planner.html');
 });
 
 app.listen(process.env.PORT || 5000, function () {
-  console.log("Server is running");
+	console.log('Server is running');
 });
