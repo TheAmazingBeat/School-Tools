98428e23af1c0b3b5e29162d98fae0f0815f7a21
TheAmazingBeat <ice.baldivas@gmail.com>
2022-02-13 00:18:21 -0600

    ES6 Syntax

--- public/js/planner.js
+++ public/js/planner.js
@@ -1,58 +1,59 @@
 const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December",
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
 ];
 let d = new Date();
 let monthNum = d.getMonth();
 let yearNum = d.getFullYear();
-let $calendar = $("#monthYear");
+let calendarName = '#monthYear';
 let animateDelay = 1500;
-let days;
 
 // Beginning at the current month and year
-$(document).ready(function() {
-	$calendar.text(getTheMonth(monthNum) + " " + yearNum);
-	// dateLog();
+$(document).ready(() => {
+	$('#prevArrow').click(previous);
+	$('#nextArrow').click(next);
+	$('#addEvent').click(addCalendarEvent);
+	$(calendarName).text(getTheMonth(monthNum) + ' ' + yearNum);
 	loadCalendarDays();
 	highlightToday();
 });
 
 // Highlight current date
-function highlightToday() {
-	let dateString = (d.getMonth() + 1) + "-" + d.getDate() + "-" + yearNum;
-	let idStem = "calendarMonthDayYear_";
-	let selector = "#" + idStem + dateString;
+const highlightToday = () => {
+	let dateString = d.getMonth() + 1 + '-' + d.getDate() + '-' + yearNum;
+	let idStem = 'calendarMonthDayYear_';
+	let selector = '#' + idStem + dateString;
 	console.log("Today's Date DOM Selector: " + selector);
-	$(selector).addClass("today");
-}
+	$(selector).addClass('today');
+};
 
-function dateLog() {
-	console.log("%c Date:", "color: green; font-weight: bold;");
+const dateLog = () => {
+	console.log('%c Date:', 'color: green; font-weight: bold;');
 	console.log({
 		monthNum,
 		yearNum,
 	});
-}
+};
 
 // Get month from months array
-function getTheMonth(num) {
+const getTheMonth = (num) => {
 	if (num < 0) num = 11;
 	else if (num > 11) num = 0;
 
 	return months[num];
-}
+};
 
-function previous() {
+const previous = () => {
 	animateDelay = 0;
 	// Indicates year before
 	if (getTheMonth(monthNum - 1) == months[11]) yearNum--;
@@ -61,23 +62,23 @@ function previous() {
 	if (monthNum - 1 < 0) {
 		//December <- January
 		monthNum = 11;
-		$calendar.text(getTheMonth(monthNum) + " " + yearNum);
+		$(calendarName).text(getTheMonth(monthNum) + ' ' + yearNum);
 	} else {
-		$calendar.text(getTheMonth(monthNum - 1) + " " + yearNum);
+		$(calendarName).text(getTheMonth(monthNum - 1) + ' ' + yearNum);
 		monthNum--;
 	}
 
-	$calendar.attr("class", "animate__animated animate__fadeInLeft");
-	$calendar.on("animationend", function() {
-		$calendar.attr("class", "");
+	$(calendarName).attr('class', 'animate__animated animate__fadeInLeft');
+	$(calendarName).on('animationend', () => {
+		$(calendarName).attr('class', '');
 	});
 
 	dateLog();
 	loadCalendarDays();
 	highlightToday();
-}
+};
 
-function next() {
+const next = () => {
 	animateDelay = 0;
 	// Indicates new year
 	if (getTheMonth(monthNum + 1) == months[0]) yearNum++;
@@ -86,31 +87,31 @@ function next() {
 	if (monthNum + 1 > 11) {
 		//December -> January
 		monthNum = 0;
-		$calendar.text(getTheMonth(monthNum) + " " + yearNum);
+		$(calendarName).text(getTheMonth(monthNum) + ' ' + yearNum);
 	} else {
-		$calendar.text(getTheMonth(monthNum + 1) + " " + yearNum);
+		$(calendarName).text(getTheMonth(monthNum + 1) + ' ' + yearNum);
 		monthNum++;
 	}
 
-	$calendar.attr("class", "animate__animated animate__fadeInRight");
-	$calendar.on("animationend", function() {
-		$calendar.attr("class", "");
+	$(calendarName).attr('class', 'animate__animated animate__fadeInRight');
+	$(calendarName).on('animationend', () => {
+		$(calendarName).attr('class', '');
 	});
 
 	dateLog();
 	loadCalendarDays();
 	highlightToday();
-}
+};
 
 // Returns the number of days in the month
-function numOfDays(month, year) {
+const numOfDays = (month, year) => {
 	let d = new Date(year, month + 1, 0);
 	return d.getDate();
-}
+};
 
-function loadCalendarDays() {
-	$("#calendarDays").html("");
-	days = 0;
+const loadCalendarDays = () => {
+	$('#calendarDays').html('');
+	let days = 0;
 
 	let tmpDate = new Date(yearNum, monthNum, 1);
 	// Gets how many days in the month
@@ -118,21 +119,21 @@ function loadCalendarDays() {
 	// Gets the first day of the month
 	let dayOfWeek = tmpDate.getDay();
 
-	console.log("%c loadCalendarDays():", "color:blue; font-weight:bold;");
-	console.log({
-		tmpDate,
-		num,
-		dayOfWeek,
-	});
+	// console.log('%c loadCalendarDays():', 'color:blue; font-weight:bold;');
+	// console.log({
+	// 	tmpDate,
+	// 	num,
+	// 	dayOfWeek,
+	// });
 
 	// create day prefixes before first day of the month
 	for (let i = dayOfWeek; i > 0; i--) {
-		createDayCells("blank-begin", i, tmpDate);
+		createDayCells('blank-begin', i, tmpDate);
 		days++;
 	}
 	// creates rest of the days in the month
 	for (let i = 0; i < num; i++) {
-		createDayCells("real", i);
+		createDayCells('real', i);
 		days++;
 	}
 	// create blank days after last day of the month
@@ -143,125 +144,126 @@ function loadCalendarDays() {
 
 	if (days != 35) {
 		for (let i = 0; i < daysLeft; i++) {
-			createDayCells("blank-end", i);
+			createDayCells('blank-end', i);
 		}
 	}
-}
+};
 
 // Makes days in the calendar
-function createDayCells(type, index) {
-	let idStem = "calendarMonthDayYear_";
-	let id = "";
+const createDayCells = (type, index) => {
+	let idStem = 'calendarMonthDayYear_';
+	let id = '';
 
 	// Base day cell
-	let d = document.createElement("div");
-	$(d).addClass("day animate__animated animate__fadeIn");
-	$(d).css("animation-delay", animateDelay.toString() + "ms");
-	$(d).attr("data-bs-toggle", "modal");
-	$(d).attr("data-bs-target", "#dayDetails");
+	let d = document.createElement('div');
+	$(d).addClass('day animate__animated animate__fadeIn');
+	$(d).css('animation-delay', animateDelay.toString() + 'ms');
+	$(d).attr('data-bs-toggle', 'modal');
+	$(d).attr('data-bs-target', '#dayDetails');
 
 	// Blank cells before the current month
-	if (type == "blank-begin") {
+	if (type == 'blank-begin') {
 		let daysBefore = numOfDays(monthNum - 1, yearNum) - (index - 1);
-		id = monthNum + "-" + daysBefore + "-" + yearNum;
-		$(d).attr("id", idStem + id);
-		$(d).addClass("blank");
-		$(d).attr("data-bs-whatever", id);
+		id = monthNum + '-' + daysBefore + '-' + yearNum;
+		$(d).attr('id', idStem + id);
+		$(d).addClass('blank');
+		$(d).attr('data-bs-whatever', id);
 
 		// text inside div
-		let n = document.createElement("div");
-		$(n).attr("class", "day-num");
+		let n = document.createElement('div');
+		$(n).attr('class', 'day-num');
 		$(n).text(daysBefore);
 		$(d).append(n);
 	}
 
 	// Blank cells after the current month
-	if (type == "blank-end") {
-		id = monthNum + 2 + "-" + (index + 1) + "-" + yearNum;
-		$(d).attr("id", idStem + id);
-		$(d).addClass("blank");
-		$(d).attr("data-bs-whatever", id);
+	if (type == 'blank-end') {
+		id = monthNum + 2 + '-' + (index + 1) + '-' + yearNum;
+		$(d).attr('id', idStem + id);
+		$(d).addClass('blank');
+		$(d).attr('data-bs-whatever', id);
 
 		// text inside div
 		let daysAfter = index + 1;
-		let n = document.createElement("div");
-		$(n).attr("class", "day-num");
+		let n = document.createElement('div');
+		$(n).attr('class', 'day-num');
 		$(n).text(daysAfter);
 		$(d).append(n);
 	}
 
 	//days in the current month
-	if (type == "real") {
-		id = monthNum + 1 + "-" + (index + 1) + "-" + yearNum;
-		$(d).attr("id", idStem + id);
-		$(d).attr("data-bs-whatever", id);
+	if (type == 'real') {
+		id = monthNum + 1 + '-' + (index + 1) + '-' + yearNum;
+		$(d).attr('id', idStem + id);
+		$(d).attr('data-bs-whatever', id);
 
 		// text inside div
 		let dayNum = index + 1;
-		let n = document.createElement("div");
-		$(n).attr("class", "day-num");
+		let n = document.createElement('div');
+		$(n).attr('class', 'day-num');
 		$(n).text(dayNum);
 		$(d).append(n);
 	}
 
-	$("#calendarDays").append(d);
+	$('#calendarDays').append(d);
 	animateDelay += 20;
-}
+};
 
 // Day Details when clicked on a day
-let $modal = $("#dayDetails");
-$modal.on("show.bs.modal", function(event) {
+// let $modal = $('#dayDetails');
+$('#dayDetails').on('show.bs.modal', (event) => {
+	console.log(event.relatedTarget);
+	console.log(event.currentTarget);
+	formatDayModal(event);
+});
+
+const formatDayModal = (event) => {
 	/// Div that triggered the modal
 	let button = event.relatedTarget;
 
 	/// Extract info from data-bs-* attributes
-	let date = button.getAttribute("data-bs-whatever");
+	let date = button.getAttribute('data-bs-whatever');
 
 	/// Update the modal's content.
-	let $modalTitle = $(".modal-title");
-
 	/// Date formatting on modal header
 	let formatDate = () => {
 		let someDate = date.toString();
 		let month = someDate.substring(0, someDate.indexOf('-'));
-		let day = someDate.substring((date.indexOf('-') + 1), someDate.indexOf('-', someDate.indexOf('-') + 1))
-		let year = someDate.substring(someDate.indexOf('-', (someDate.indexOf('-') + 1)) + 1);
+		let day = someDate.substring(
+			date.indexOf('-') + 1,
+			someDate.indexOf('-', someDate.indexOf('-') + 1)
+		);
+		let year = someDate.substring(someDate.indexOf('-', someDate.indexOf('-') + 1) + 1);
 
 		return getTheMonth(month - 1) + ' ' + day + ', ' + year;
-	}
-	$modalTitle.text(formatDate);
+	};
+	$('.modal-title').text(formatDate);
 
 	displayStoredHW(date);
-});
+};
 
 // Add Event
 let dayEvent = {
-	name: "",
-	date: "",
+	name: '',
+	date: '',
 };
 
 // Add Event Form
-function addCalendarEvent() {
-
-}
-
+const addCalendarEvent = () => {};
 
 // Stored HW
 let homeworks = JSON.parse(localStorage.getItem('homeworks'));
 // Display Homeworks from Prioritizer
-function displayStoredHW(selectedDate) {
+const displayStoredHW = (selectedDate) => {
 	if (homeworks != null) {
 		$('#schoolWorkList').text('');
-		// $('#schoolWorkList').text(homeworks);
 		console.log('Selected Date: ' + selectedDate);
 		/// Analyze/Format objects
 		for (let i = 0; i < homeworks.length; i++) {
 			let schoolWorkItem = document.createElement('li');
 			$(schoolWorkItem).addClass('school-work-item');
 			$(schoolWorkItem).text(homeworks[i].name + ' (' + homeworks[i].type + ' Grade' + ')');
-			if (homeworks[i].date == selectedDate)
-				$('#schoolWorkList').append(schoolWorkItem);
+			if (homeworks[i].dueDate == selectedDate) $('#schoolWorkList').append(schoolWorkItem);
 		}
 	}
-
-}
+};
