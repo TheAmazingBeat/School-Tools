54419d792e41dbf6accd37203edd876f6cb9ffa3
TheAmazingBeat <ice.baldivas@gmail.com>
2022-02-20 23:01:16 -0600

    Installed axios

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/favicon.ico', function (req, res) {
+app.get('/favicon.ico', (req, res) => {
 	res.sendFile(__dirname + '/favicon.ico');
 });
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/prioritizer', function (req, res) {
+app.get('/prioritizer', (req, res) => {
 	res.sendFile(__dirname + '/public/src/prioritizer.html');
 });
 
-app.get('/calculator', function (req, res) {
+app.get('/calculator', (req, res) => {
 	res.sendFile(__dirname + '/public/src/calculator.html');
 });
 
-app.get('/planner', function (req, res) {
+app.get('/planner', (req, res) => {
 	res.sendFile(__dirname + '/public/src/planner.html');
 });
 
 const port = 3000;
-app.listen(process.env.PORT || port, function () {
+app.listen(process.env.PORT || port, () => {
 	console.log(`Server is running at http://localhost:${port}`);
 });
 
 //TODO Get Calendar Holidays through an API
 //TODO Store homeworks in a database and display in Prioritizer when page loads (instead of localStorage)
 //TODO Store Events in a database and display in Planner when page loads (instead of localStorage)
+
+// CANVAS LMS
+const canvas_user_token = '5604~bGH7g0L4oUWTCVag0m4DmZ9FpIokztG3c0OBvHyAZz5v7m2p8Y3N3QVYISecfqsK';
+const course_id = 73509;
+app.get('/poop', (req, res) => {
+	const URL = `https://garlandisd.instructure.com/api/v1/courses/${course_id}/assignments?access_token=${canvas_user_token}`;
+	axios
+		.get(URL)
+		.then((response) => {
+			console.log(response);
+			res.send(response);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
